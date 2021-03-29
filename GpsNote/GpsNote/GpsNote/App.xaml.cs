9da152fcfc2cd46ac3670f249ec20b3009a8b4e5
Dmitrij ermoshkin <DmitrijErmoshkin@gmail.com>
2021-03-29 06:00:16 -0700

    first build

--- GpsNote/GpsNote/GpsNote/App.xaml.cs
+++ GpsNote/GpsNote/GpsNote/App.xaml.cs
@@ -1,28 +1,33 @@
-﻿using System;
+using GpsNote.ViewModels;
+using GpsNote.Views;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace GpsNote
 {
-    public partial class App : Application
+    public partial class App
     {
-        public App()
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
         }
 
-        protected override void OnStart()
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-        }
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
 
-        protected override void OnSleep()
-        {
-        }
-
-        protected override void OnResume()
-        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
         }
     }
 }
