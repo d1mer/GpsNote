a53bc795ccb274264da4b1336c7d2b09caa53e6f
Dmitrij ermoshkin <DmitrijErmoshkin@gmail.com>
2021-04-08 00:45:33 -0700

    fix

--- GpsNote/GpsNote/GpsNote/Resource/Resource.Designer.cs
+++ GpsNote/GpsNote/GpsNote/Resource/Resource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GpsNote.Resx {
+namespace GpsNote.Resource {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GpsNote.Resx {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GpsNote.Resx.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GpsNote.Resource.Resource", typeof(Resource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
