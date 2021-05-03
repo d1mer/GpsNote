9d100893882a27343248288c45a46554763b3ce3
d1mer <d1mer@outlook.com>
2021-05-03 23:57:24 +0300

    save

---
+++ GpsNote/GpsNote/GpsNote.Android/ExtendedTabbedPageRenderer.cs
@@ -0,0 +1,109 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.View;
+using Android.Views;
+using Android.Widget;
+using Google.Android.Material.Tabs;
+using GpsNote.Controls;
+using GpsNote.Droid;
+using GpsNote.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(MainTabbedPage), typeof(ExtendedTabbedPageRenderer))]
+namespace GpsNote.Droid
+{
+    public class ExtendedTabbedPageRenderer : TabbedPageRenderer
+    {
+        private TabLayout _TabBar;
+        protected TabLayout TabBar => _TabBar ?? SearchInChildren<TabLayout>();
+
+        private ViewPager _Pager;
+        protected ViewPager Pager => _Pager ?? SearchInChildren<ViewPager>();
+
+        protected IVisualElementRenderer _tabBarRenderer;
+
+        public ExtendedTabbedPageRenderer(Context context) : base(context)
+        {
+        }
+
+        #region -- Overrides --
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null && _tabBarRenderer is null)
+            {
+                var tabbedPage = Element as ExtendedTabbedPage;
+                _tabBarRenderer = Platform.GetRenderer(tabbedPage.TabBarView);
+
+                if (_tabBarRenderer is null)
+                {
+                    _tabBarRenderer = Platform.CreateRendererWithContext(tabbedPage.TabBarView, Context);
+                    Platform.SetRenderer(tabbedPage.TabBarView, _tabBarRenderer);
+                }
+
+                AddView(_tabBarRenderer.View);
+
+                TabBar.Visibility = Android.Views.ViewStates.Gone;
+            }
+        }
+
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+            TabBar.Visibility = Android.Views.ViewStates.Gone;
+
+            var tabbedPage = Element as ExtendedTabbedPage;
+
+            if (tabbedPage.TabBarPosition == ExtendedTabbedPage.TabBarPositionType.Top)
+            {
+                base.OnLayout(changed, l, t + (int)(tabbedPage.TabBarHeight * Context.Resources.DisplayMetrics.Density), r, b);
+
+                Pager.Layout(l, t + (int)(tabbedPage.TabBarHeight * Context.Resources.DisplayMetrics.Density), r, b);
+
+                Xamarin.Forms.Layout.LayoutChildIntoBoundingRegion(_tabBarRenderer.Element,
+                    new Rectangle(l, t, Context.FromPixels(r - l), tabbedPage.TabBarHeight));
+            }
+            else
+            {
+                base.OnLayout(changed, l, t, r, b - +(int)(tabbedPage.TabBarHeight * Context.Resources.DisplayMetrics.Density));
+
+                Pager.Layout(l, t, r, b - +(int)(tabbedPage.TabBarHeight * Context.Resources.DisplayMetrics.Density));
+
+                Xamarin.Forms.Layout.LayoutChildIntoBoundingRegion(_tabBarRenderer.Element,
+                    new Rectangle(l, Context.FromPixels(b - t) - tabbedPage.TabBarHeight, Context.FromPixels(r - l), tabbedPage.TabBarHeight));
+            }
+
+            _tabBarRenderer.UpdateLayout();
+        }
+
+        #endregion
+
+        #region -- Private Helpers --
+
+        private T SearchInChildren<T>() where T : class
+        {
+            T res = null;
+
+            for (int i = 0; i < ChildCount; i++)
+            {
+                if (GetChildAt(i) is T child)
+                {
+                    res = child;
+                }
+            }
+
+            return res;
+        }
+
+        #endregion
+    }
+}
---
+++ GpsNote/GpsNote/GpsNote/Controls/ExtendedTabbedPage.cs
@@ -0,0 +1,277 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace GpsNote.Controls
+{
+    public class ExtendedTabbedPage : TabbedPage
+    {
+        public enum TabBarPositionType
+        {
+            Top,
+            Bottom
+        };
+
+        #region -- Public Properties --
+
+        public static readonly BindableProperty TabBarPositionProperty = BindableProperty.Create(
+            nameof(TabBarPosition),
+            typeof(TabBarPositionType),
+            typeof(ExtendedTabbedPage),
+            TabBarPositionType.Top);
+
+        public TabBarPositionType TabBarPosition
+        {
+            get => (TabBarPositionType)GetValue(TabBarPositionProperty);
+            set => SetValue(TabBarPositionProperty, value);
+        }
+
+        public static readonly BindableProperty TabBarCellTemplateProperty = BindableProperty.Create(
+            nameof(TabBarCellTemplate),
+            typeof(DataTemplate),
+            typeof(ExtendedTabbedPage));
+
+        public DataTemplate TabBarCellTemplate
+        {
+            get => (DataTemplate)GetValue(TabBarCellTemplateProperty);
+            set => SetValue(TabBarCellTemplateProperty, value);
+        }
+
+        public static readonly BindableProperty TabBarSelectedCellTemplateProperty = BindableProperty.Create(
+            nameof(TabBarSelectedCellTemplate),
+            typeof(DataTemplate),
+            typeof(ExtendedTabbedPage));
+
+        public DataTemplate TabBarSelectedCellTemplate
+        {
+            get => (DataTemplate)GetValue(TabBarSelectedCellTemplateProperty);
+            set => SetValue(TabBarSelectedCellTemplateProperty, value);
+        }
+
+        public static readonly BindableProperty SplitterColorProperty = BindableProperty.Create(
+            nameof(SplitterColor),
+            typeof(Color),
+            typeof(ExtendedTabbedPage),
+            Color.LightGray);
+
+        public Color SplitterColor
+        {
+            get => (Color)GetValue(SplitterColorProperty);
+            set => SetValue(SplitterColorProperty, value);
+        }
+
+        public static readonly BindableProperty SplitterWidthProperty = BindableProperty.Create(
+            nameof(SplitterWidth),
+            typeof(double),
+            typeof(ExtendedTabbedPage),
+            1.0);
+
+        public double SplitterWidth
+        {
+            get => (double)GetValue(SplitterWidthProperty);
+            set => SetValue(SplitterWidthProperty, value);
+        }
+
+        public static readonly BindableProperty TopBarColorProperty = BindableProperty.Create(
+            nameof(TopBarColor),
+            typeof(Color),
+            typeof(ExtendedTabbedPage),
+            Color.LightGray);
+
+        public Color TopBarColor
+        {
+            get => (Color)GetValue(TopBarColorProperty);
+            set => SetValue(TopBarColorProperty, value);
+        }
+
+        public static readonly BindableProperty TopBarHeightProperty = BindableProperty.Create(
+            nameof(TopBarHeight),
+            typeof(double),
+            typeof(ExtendedTabbedPage),
+            1.0);
+
+        public double TopBarHeight
+        {
+            get => (double)GetValue(TopBarHeightProperty);
+            set => SetValue(TopBarHeightProperty, value);
+        }
+
+        public static readonly BindableProperty BottomBarColorProperty = BindableProperty.Create(
+            nameof(BottomBarColor),
+            typeof(Color),
+            typeof(ExtendedTabbedPage),
+            Color.LightGray);
+
+        public Color BottomBarColor
+        {
+            get => (Color)GetValue(BottomBarColorProperty);
+            set => SetValue(BottomBarColorProperty, value);
+        }
+
+        public static readonly BindableProperty BottomBarHeightProperty = BindableProperty.Create(
+            nameof(BottomBarHeight),
+            typeof(double),
+            typeof(ExtendedTabbedPage),
+            1.0);
+
+        public double BottomBarHeight
+        {
+            get => (double)GetValue(BottomBarHeightProperty);
+            set => SetValue(BottomBarHeightProperty, value);
+        }
+
+        public static readonly BindableProperty TabBarHeightProperty = BindableProperty.Create(
+            nameof(TabBarHeight),
+            typeof(double),
+            typeof(ExtendedTabbedPage),
+            70.0);
+
+        public double TabBarHeight
+        {
+            get => (double)GetValue(TabBarHeightProperty);
+            set => SetValue(TabBarHeightProperty, value);
+        }
+
+        public Grid TabBarView
+        {
+            get
+            {
+                if (_tabBarView == null)
+                {
+                    CreateTabBar();
+                }
+
+                return _tabBarView;
+            }
+        }
+
+        #endregion
+
+        #region -- Overrides --
+
+        protected override void OnCurrentPageChanged()
+        {
+            base.OnCurrentPageChanged();
+
+            if (selectedCells != null)
+            {
+                for (int i = 0; i < Children.Count; i++)
+                {
+                    cells[i].IsVisible = Children[i] != CurrentPage;
+                    selectedCells[i].IsVisible = !cells[i].IsVisible;
+                }
+            }
+        }
+
+        #endregion
+
+        #region -- Protected Helpers --
+
+        protected Grid _tabBarView = null;
+
+        protected List<View> cells;
+        protected List<View> selectedCells;
+
+        protected void CreateTabBar()
+        {
+            _tabBarView = new Grid
+            {
+                HeightRequest = TabBarHeight,
+                RowSpacing = 0
+            };
+
+            _tabBarView.RowDefinitions.Add(new RowDefinition
+            {
+                Height = new GridLength(TopBarHeight, GridUnitType.Absolute)
+            });
+            _tabBarView.RowDefinitions.Add(new RowDefinition
+            {
+                Height = new GridLength(1, GridUnitType.Star)
+            });
+            _tabBarView.RowDefinitions.Add(new RowDefinition
+            {
+                Height = new GridLength(BottomBarHeight, GridUnitType.Absolute)
+            });
+
+            _tabBarView.Children.Add(new BoxView
+            {
+                BackgroundColor = TopBarColor
+            }, 0, 0);
+
+            _tabBarView.Children.Add(new BoxView
+            {
+                BackgroundColor = BottomBarColor
+            }, 0, 2);
+
+            cells = new List<View>();
+            if (TabBarSelectedCellTemplate != null)
+            {
+                selectedCells = new List<View>();
+            }
+
+            Grid gridTabs = new Grid()
+            {
+                ColumnSpacing = 0
+            };
+
+            int i = 0;
+
+            foreach (Page page in Children)
+            {
+                if (i > 0)
+                {
+                    gridTabs.ColumnDefinitions.Add(new ColumnDefinition
+                    {
+                        Width = new GridLength(SplitterWidth, GridUnitType.Absolute)
+                    });
+                    gridTabs.Children.Add(new BoxView
+                    {
+                        BackgroundColor = SplitterColor
+                    }, 2 * i - 1, 0);
+                }
+
+                gridTabs.ColumnDefinitions.Add(new ColumnDefinition
+                {
+                    Width = new GridLength(1, GridUnitType.Star)
+                });
+
+                View cell = (View)TabBarCellTemplate.CreateContent();
+                cell.BindingContext = page;//.BindingContext ?? page;
+                cell.GestureRecognizers.Add(new TapGestureRecognizer
+                {
+                    CommandParameter = cell,
+                    Command = SelectCellCommand
+                });
+                gridTabs.Children.Add(cell, 2 * i, 0);
+                cells.Add(cell);
+
+                if (TabBarSelectedCellTemplate != null)
+                {
+                    View selectedCell = (View)TabBarSelectedCellTemplate.CreateContent();
+                    selectedCell.BindingContext = cell.BindingContext;
+                    selectedCell.IsVisible = false;
+                    gridTabs.Children.Add(selectedCell, 2 * i, 0);
+                    selectedCells.Add(selectedCell);
+                }
+
+                i++;
+            }
+
+            _tabBarView.Children.Add(gridTabs, 0, 1);
+
+            OnCurrentPageChanged();
+        }
+
+        protected ICommand SelectCellCommand => new Command((v) =>
+        {
+            int index = cells.IndexOf((View)v);
+
+            if (index >= 0)
+            {
+                CurrentPage = Children[index];
+            }
+        });
+
+        #endregion
+    }
+}
